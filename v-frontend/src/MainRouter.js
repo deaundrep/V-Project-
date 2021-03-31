09d093d9f028dd9cb1f588a788caa76f327ee63b
DeAundre Prescott <deaundre.prescott@codeimmersives.com>
2021-03-31 15:38:08 -0400

    component files

--- v-frontend/src/MainRouter.js
+++ v-frontend/src/MainRouter.js
@@ -1,1 +1,18 @@
 import React from "react";
+
+import Home from "./component/Home";
+import Login from "./component/Login/Login";
+import SignUp from "./component/SignUp/SignUp";
+import Profile from "./component/Profile/Profile";
+import Navbar from "./component/Navbar/Navbar";
+import PrivateRoute from "./component/PrivateRoute/PrivateRoute";
+
+
+
+
+const MainRouter = (props) => {
+    return
+}
+
+
+export default MainRouter;
