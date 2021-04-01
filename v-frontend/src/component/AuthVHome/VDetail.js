b8398a3f572217fb72b85dcb8f053dd9ef2db56b
DeAundre Prescott <deaundre.prescott@codeimmersives.com>
2021-04-01 18:25:01 -0400

    login

--- v-frontend/src/component/AuthVHome/VDetail.js
+++
@@ -1,5 +0,0 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import axios from "axios";
-const CancelToken = axios.CancelToken;
-const source = CancelToken.source();
